d0e225caca214d0314eb6973fe17c55b711138ae
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-14 12:21:10 -0600

    mvp done

--- main.rb
+++ main.rb
@@ -1,24 +1,38 @@
-students = ["Leveille, Andre", "AshWorth, Ryan",
- "Biddinger, Caden", "Mbia, Arsene", "Boyden, Lindsay",
-  "DelaCruz, Joey", "Dunshee, Kyle", "Riches, James",
-  "Smith, Shawn", "Sultan, Maxwell", "Zarate, Javier"]
+students = ["Leveille, Andre", "AshWorth, Ryan", "Biddinger, Caden", "Mbia, Arsene", "Boyden, Lindsay", "DelaCruz, Joey", "Dunshee, Kyle", "Riches, James", "Smith, Shawn", "Sultan, Maxwell", "Zarate, Javier"]
+# my task now is to create a group randomizer
+# create a new list w array of hashes with keys fist_name and last_name
+students_formated = students.map do |student|
+  # need to seperate out first and last from original string
+  name_arr = student.split(",")
+  first = name_arr[1].strip
+  last = name_arr[0].strip
+  { first_name: first, last_name: last }
+end 
 
-  # my task is to create a group randomizer
-  students_formated = students.map do |student|
-    #need to separate out first and last name from original string
-    name_arr = student.split(',')
-    first = name_arr[1].strip
-    last = name_arr[0].strip
+# with the newly create array i want to randomly select people and pull and assing into groups
+#shuffle stundents
+students_formated.shuffle!
+# {name: 'group1', memebers: [students_formated.pop, students_formated.pop ]}
+groups = []
+5.times do |i|
+  groups.push({ name: "group#{i + 1}", members: [students_formated.pop, students_formated.pop] })
+end
 
-    {first_name: first, last_name: last}
+# here we need to handle last person since we have a odd number
+# randomly select a group and and add last student to members
+index = rand(groups.size)
+groups[index][:members].push(students_formated.pop)
+# p groups
+# p students_formated.size
+# print outadd groups in a slack friendly format
+
+groups.each do |group|
+  puts group[:name]
+  # this an array of members
+  puts "--------"
+  group[:members].each do |member|
+    puts "#{member[:first_name]} #{member[:last_name]}"
   end
-
-  p students_formated
-
-  #create a new array of hashes with keys first_name and last_name
-
-
-  #with the newly created array i want to randomly people and pull and 
-  #assign into group
-
-  #print out groups in a slack friendly format
+  puts "xxxxxx"
+end
+# changed main.rb

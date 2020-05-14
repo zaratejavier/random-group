255d54e3a2e2bdffde08b8844a32104d1eafb833
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-14 11:07:51 -0600

    stubbed out comments for pproject

---
+++ main.rb
@@ -0,0 +1,13 @@
+students = ["Leveille, Andre", "AshWorth, Ryan",
+ "Biddinger, Caden", "Mbia, Arsene", "Boyden, Lindsay",
+  "DelaCruz, Joey", "Dunshee, Kyle", "Riches, James",
+  "Smith, Shawn", "Sultan, Maxwell", "Zarate, Javier"]
+
+  # my task is to create a group randomizer
+
+  #create a new array of hashes with keys first_name and last_name
+
+  #with the newly created array i want to randomly people and pull and 
+  #assign into group
+
+  #print out groups in a slack friendly format

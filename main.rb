231222e075a1fbe58ead445b8ff631e48dd8f450
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-14 11:22:53 -0600

    formated array

--- main.rb
+++ main.rb
@@ -4,8 +4,19 @@ students = ["Leveille, Andre", "AshWorth, Ryan",
   "Smith, Shawn", "Sultan, Maxwell", "Zarate, Javier"]
 
   # my task is to create a group randomizer
+  students_formated = students.map do |student|
+    #need to separate out first and last name from original string
+    name_arr = student.split(',')
+    first = name_arr[1].strip
+    last = name_arr[0].strip
+
+    {first_name: first, last_name: last}
+  end
+
+  p students_formated
 
   #create a new array of hashes with keys first_name and last_name
+
 
   #with the newly created array i want to randomly people and pull and 
   #assign into group
